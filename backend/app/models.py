d1d0ba781c3753d6fe1d752da0390182acb7f55b
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-09-17 18:33:31 +0100

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,15 @@
-models.py
+# backend/app/models.py
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Document(Base):
+    __tablename__ = "documents"
+
+    id = Column(Integer, primary_key=True, index=True)
+    file_name = Column(String)
+    file_path = Column(String)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+
+    vehicle = relationship("Vehicle", back_populates="documents")
+
