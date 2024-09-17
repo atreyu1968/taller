a0b281b138266da1d223ccea0dda7ba9ead1f7b6
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-09-17 18:35:43 +0100

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,1 +1,18 @@
+// frontend/src/components/ProtectedRoute.js
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const token = localStorage.getItem('token');
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        token ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
 
