c72c7ab631fd059e8d8e209a081abfb64ff628f3
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-09-17 18:34:07 +0100

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,1 +1,24 @@
-schemas.py
+# backend/app/schemas.py
+from pydantic import BaseModel
+from typing import List
+
+class DocumentOut(BaseModel):
+    id: int
+    file_name: str
+    file_path: str
+
+    class Config:
+        orm_mode = True
+
+class VehicleOut(BaseModel):
+    id: int
+    plate_number: str
+    brand: str
+    model: str
+    entry_date: str
+    status: str
+    documents: List[DocumentOut] = []
+
+    class Config:
+        orm_mode = True
+
