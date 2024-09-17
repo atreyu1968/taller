1bf6a7c67a75a1c84f707cb6a9883c6b830552a7
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-09-17 18:33:00 +0100

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,1 +1,11 @@
-database.py
+# backend/app/database.py
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:password@db/taller"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
